c5b435bb1191be472e524ec908e8cb80e6e2f414
SergioZamoras <sergioruizygarcia@gmail.com>
2018-06-07 13:54:23 +0200

    Mejorando código pero ay que ver los problemas que tengo con Firebase

--- app/src/main/java/com/android/teaching/chatapp/ChatActivity.java
+++ app/src/main/java/com/android/teaching/chatapp/ChatActivity.java
@@ -1,48 +1,54 @@
 package com.android.teaching.chatapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 
 public class ChatActivity extends AppCompatActivity {
+
+    private FirebaseListAdapter<NewMessageActivity> adapter;
+    RelativeLayout activity_chat;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
     }
-    public void onClick(View view) {
 
-    }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("ChatActivity", "onStart");
-    }
+    private void displayChatMessage() {
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("ChatActivity", "onResume");
-    }
+        ListView listOfMessage = (ListView) findViewById(R.id.list_of_message);
+        adapter = new FirebaseListAdapter<NewMessageActivity>(this, NewMessageActivity.class, R.layout.activity_new_message,
+                FirebaseDatabase.getInstance().getReference()) {
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("ChatActivity", "onPause");
-    }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("ChatActivity", "onStop");
-    }
+            protected void NewMessageActivity(View v, NewMessageActivity model, int position) {
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("ChatActivity", "onDestroy");
+
+                TextView messageText, messageUser;
+                messageText = (TextView) v.findViewById(R.id.message_text);
+                messageUser = (TextView) v.findViewById(R.id.message_user);
+
+
+                messageText.setText(model.getMessageText());
+                messageUser.setText(model.getMessageUser());
+
+            }
+        };
+        listOfMessage.setAdapter(adapter);
     }
 }
+
+
+
--- app/src/main/java/com/android/teaching/chatapp/NewMessageActivity.java
+++ app/src/main/java/com/android/teaching/chatapp/NewMessageActivity.java
@@ -2,8 +2,14 @@ package com.android.teaching.chatapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import com.google.firebase.database.FirebaseDatabase;
 
 public class NewMessageActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,13 +19,33 @@ public class NewMessageActivity extends AppCompatActivity {
 
         private String messageText;
         private String messageUser;
-        private long messageTime;
+
 
         public NewMessageActivity(String messageText, String messageUser) {
             this.messageText = messageText;
             this.messageUser = messageUser;
 
 
+
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    EditText input = (EditText)findViewById(R.id.input);
+
+                    //A saber que ostia me falta aquí
+
+                    FirebaseDatabase.getInstance()
+                            .getReference()
+                            .push()
+                            .setValue(new NewMessageActivity(input.getText().toString(),
+                                    FirebaseAuth.getInstance()
+                                            .getCurrentUser()
+                                            .getDisplayName())
+                            );
+
+                    input.setText("");
+                }
+            });
         }
 
 
@@ -39,12 +65,5 @@ public class NewMessageActivity extends AppCompatActivity {
             this.messageUser = messageUser;
         }
 
-        public long getMessageTime() {
-            return messageTime;
-        }
-
-        public void setMessageTime(long messageTime) {
-            this.messageTime = messageTime;
-        }
     }
 
